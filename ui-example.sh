3173710b8f63f81e4a992d0273f626a3806ff154
Natalia <nataliadubon@ucsb.edu>
2021-12-15 11:06:49 -0800

    Adding more about read

--- ui-example.sh
+++ ui-example.sh
@@ -1,5 +1,5 @@
-#'read' command requests user to input text
 echo "What is your name?"
 read name
 echo "Hello $name."
+#the read command requests user input, we use `$` to recall the variable established by `read`
 
